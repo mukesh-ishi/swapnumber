45e3d99cff6de640270bd57ef5ae357c123879e2
mukeshishi <mukesh.ishi@perennialsys.com>
2022-05-27 18:36:13 +0530

    hii

---
+++ src/SwapNumber.java
@@ -0,0 +1,22 @@
+public class SwapNumber {
+    public static void main(String[] args) {
+//        int i=10;
+//        int j=20;
+//
+//         i=i+j;
+//         j=i-j;
+//         i=i-j;
+//
+//        System.out.println(i);
+//        System.out.println(j);
+//
+//        System.out.println("A".compareTo("Z"));
+
+
+//
+        String str="$java!&star#$%";
+        String plainstr=str.replaceAll("[^a-zA-Z0-9]","");
+        System.out.println(plainstr);
+
+    }
+}
